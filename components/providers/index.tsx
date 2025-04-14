d1bf41b16f4086e86b74ae776a35703d93d487e8
itztla <hanumanman37@gmail.com>
2025-04-14 10:56:48 +0700

    add react query

---
+++ components/providers/index.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { ReactQueryProvider } from './react-query-provider';
+import { ThemeProvider } from './theme-provider';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ReactQueryProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </ReactQueryProvider>
+  );
+}
